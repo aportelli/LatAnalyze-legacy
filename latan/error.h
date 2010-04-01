3362d71c62bf0df027a97ab32b20cda8dd978a97
Antonin Portelli <antonin@MacAnt.local>
2010-04-01 16:45:32 +0200

    global macros and variables and error handling

---
+++ latan/error.h
@@ -0,0 +1,69 @@
+#ifndef LATAN_ERROR_H_
+#define LATAN_ERROR_H_
+
+#include <latan/globals.h>
+
+__BEGIN_DECLS
+
+enum
+{
+	LATAN_SUCCESS	= 0,
+	LATAN_EFAULT	= 3,	/* invalid pointer */
+	LATAN_ENOMEM	= 8,	/* malloc error */
+	LATAN_EBADLEN	= 19,	/* matrix dimension error */
+	LATAN_ENOTSQR	= 20	/* matrix is not square error */
+};
+
+typedef void latan_error_handler_t(const char*, const char*, int, int);
+
+void
+latan_error(const char* reason, const char *file, int line, \
+			int latan_errno);
+
+latan_error_handler_t*
+latan_set_error_handler(latan_error_handler_t* new_handler);
+
+latan_error_handler_t*
+latan_set_error_handler_off(void);
+
+#define LATAN_ERROR(reason, latan_errno)\
+{\
+	latan_error(reason,__FILE__,__LINE__,latan_errno);\
+	return latan_errno;\
+}
+
+#define LATAN_ERROR_VAL(reason, latan_errno, value)\
+{\
+	latan_error(reason,__FILE__,__LINE__,latan_errno);\
+	return value;\
+}
+
+#define LATAN_ERROR_VOID(reason, latan_errno)\
+{\
+	latan_error(reason,__FILE__,__LINE__,latan_errno);\
+	return;\
+}
+
+#define LATAN_ERROR_NULL(reason, latan_errno)\
+LATAN_ERROR_VAL(reason,latan_errno,0)
+
+#define LATAN_ERROR_NORET(reason, latan_errno)\
+latan_error(reason,__FILE__,__LINE__,errno)
+
+#define LATAN_ERROR_SELECT_2(a,b)       \
+((a) != LATAN_SUCCESS ? (a) : ((b) != LATAN_SUCCESS ? (b) : LATAN_SUCCESS))
+#define LATAN_ERROR_SELECT_3(a,b,c)     \
+((a) != LATAN_SUCCESS ? (a) : LATAN_ERROR_SELECT_2(b,c))
+#define LATAN_ERROR_SELECT_4(a,b,c,d)   \
+((a) != LATAN_SUCCESS ? (a) : LATAN_ERROR_SELECT_3(b,c,d))
+#define LATAN_ERROR_SELECT_5(a,b,c,d,e) \
+((a) != LATAN_SUCCESS ? (a) : LATAN_ERROR_SELECT_4(b,c,d,e))
+#define LATAN_UPDATE_STATUS(status,instruction)\
+{\
+	int cstat;\
+	cstat=instruction;\
+	status = LATAN_ERROR_SELECT_2(status,cstat);\
+}
+__END_DECLS
+
+#endif
---
+++ latan/globals.h
@@ -0,0 +1,167 @@
+/*!
+ @file macros.h
+ @brief Miscellaneous utility macros.
+ @author <A HREF=mailto:antonin.portelli@gmail.com>Antonin Portelli</A>
+*/
+#ifndef LATAN_GLOBALS_H_
+#define LATAN_GLOBALS_H_
+
+/* macros for including from C++ code */
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS }
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
+
+/* portability of exit status */
+#ifndef EXIT_SUCCESS
+#define EXIT_SUCCESS 0
+#endif
+
+/* verbosity flags */
+#define QUIET false
+#define VERB true
+#define VVERB 2
+
+/* math */
+/*!
+ @def SQ(x)
+ @brief Macro for square operation.
+ */
+#define SQ(x) ((x)*(x))
+/*!
+ @def C_PI
+ @brief \f$\pi\f$ math constant.
+ */
+#define C_PI 3.1415926535897932384626433832795028841970
+/** 1 fm = 0.005067731 MeV^(-1) **/
+#define C_FM 0.005067731
+
+/* error handling */
+#include <latan/error.h>
+
+/* memory allocation */
+/*!
+ @def MALLOC(pt,typ,size)
+ @brief Allocate memory for an array, generates a fatal error in case of failure.
+ 
+ @param pt pointer on the first element of the array
+ @param typ pointer type of the elements of the array
+ @param size number of elements of the array
+ */
+#define MALLOC(pt,typ,size)\
+{\
+	pt = (typ)(malloc((size_t)(size)*sizeof(*pt)));\
+	if (pt == NULL)\
+	{\
+		LATAN_ERROR("memory allocation failed",LATAN_EFAULT);\
+	}\
+}
+#define MALLOC_NOERRET(pt,typ,size)\
+{\
+	pt = (typ)(malloc((size_t)(size)*sizeof(*pt)));\
+	if (pt == NULL)\
+	{\
+		LATAN_ERROR_NORET("memory allocation failed",LATAN_EFAULT);\
+	}\
+}
+/*!
+ @def REALLOC(pt,pt_old,typ,size)
+ @brief Reallocate memory for an array, backup of the old values is made. Generates a fatal error in case of failure.
+ 
+ @param pt pointer on the first element of the array
+ @param pt_old pointer on the first element of the array to backup
+ @param typ pointer type of the elements of the array
+ @param size number of elements of the array
+ */
+#define REALLOC(pt,pt_old,typ,size)\
+{\
+	pt = (typ)(realloc(pt_old,(size_t)(size)*sizeof(*pt)));\
+	if (pt == NULL)\
+	{\
+		LATAN_ERROR("memory allocation failed",LATAN_EFAULT);\
+	}\
+}
+#define REALLOC_NOERRET(pt,pt_old,typ,size)\
+{\
+	pt = (typ)(realloc(pt_old,(size_t)(size)*sizeof(*pt)));\
+	if (pt == NULL)\
+	{\
+		LATAN_ERROR_NORET("memory allocation failed",LATAN_EFAULT);\
+	}\
+}
+/*!
+ @def FREE(pt)
+ @brief Desallocate a previoulsy memory allocated array.
+ 
+ @param pt pointer on the first element of the array
+ */
+#define FREE(pt)\
+{\
+	if (pt != NULL)\
+	{\
+		free(pt);\
+		pt = NULL;\
+	}\
+}
+/* file opening */
+/*!
+ @def FOPEN(f,fname,mode)
+ @brief Macro opening a file, generates a fatal error in case of failure.
+ 
+ @param f pointer on a \b FILE structure (standard C) to access the file
+ @param fname string for the file name
+ @param mode string for the opening mode of the file (\e cf. \b fopen standard C function)
+ */
+#define FOPEN(f,fname,mode)\
+{\
+	f = fopen(fname,mode);\
+	if (f == NULL)\
+	{\
+		stringbuf errmsg;\
+		sprintf(errmsg,"error opening file %s",fname);\
+		LATAN_ERROR(errmsg,LATAN_EFAULT);\
+	}\
+}
+#define FOPEN_NOERRET(f,fname,mode)\
+{\
+	f = fopen(fname,mode);\
+	if (f == NULL)\
+	{\
+		stringbuf errmsg;\
+		sprintf(errmsg,"error opening file %s",fname);\
+		LATAN_ERROR_NORET(errmsg,LATAN_EFAULT);\
+	}\
+}
+
+__BEGIN_DECLS
+
+/* boolean type */
+typedef enum
+{
+	false = 0,
+	true = 1
+} bool;
+
+/* string buffers */
+/*!
+ @def STRING_LENGTH
+ @brief Number of characters of a string defined by the ::stringbuf type.
+*/
+#define STRING_LENGTH 256
+/*!
+ @typedef stringbuf
+ @brief String type of fixed length #STRING_LENGTH
+*/
+typedef char stringbuf[STRING_LENGTH];
+
+extern const char* latan_name;
+extern const char* latan_version;
+
+__END_DECLS
+
+#endif

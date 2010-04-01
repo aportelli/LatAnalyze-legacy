8443e588c09fc7d9f477a059e1a4faae7055e6fa
Antonin Portelli <antonin@MacAnt.local>
2010-04-01 16:46:29 +0200

    matrix type clean rewriting + example

---
+++ latan/mat.c
@@ -0,0 +1,446 @@
+#include <latan/mat.h>
+#include <latan/includes.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_blas.h>
+
+/*								allocation									*/
+/****************************************************************************/
+void mat_create(mat* m, const size_t init_nrow, const size_t init_ncol)
+{
+	gsl_error_handler_t* error_handler;
+	
+	error_handler = gsl_set_error_handler(&latan_error);
+	*m = gsl_matrix_alloc(init_nrow,init_ncol);
+	gsl_set_error_handler(error_handler);
+}
+
+void mat_create_ar(mat** m, const size_t nmat, const size_t init_nrow,\
+			   const size_t init_ncol)
+{
+	size_t i;
+	
+	MALLOC_NOERRET(*m,mat*,nmat);
+	for (i=0;i<nmat;i++)
+	{
+		mat_create((*m)+i,init_nrow,init_ncol);
+	}
+}
+
+void mat_destroy(mat* m)
+{
+	gsl_matrix_free(*m);
+}
+
+void mat_destroy_ar(mat** m, const size_t nmat)
+{
+	size_t i;
+	
+	for (i=0;i<nmat;i++)
+	{
+		mat_destroy((*m)+i);
+	}
+	FREE(*m);
+}
+
+/*								access										*/
+/****************************************************************************/
+size_t nrow(const mat m)
+{
+	return m->size1;
+}
+
+size_t ncol(const mat m)
+{
+	return m->size2;
+}
+
+double mat_get(const mat m, const size_t i, const size_t j)
+{
+	if ((i>=nrow(m))||(j>=ncol(m)))
+	{
+		LATAN_ERROR_NORET("index out of range",LATAN_EBADLEN);
+	}
+
+	return gsl_matrix_get(m,i,j);
+}
+
+void mat_set(mat m, const size_t i, const size_t j, const double val)
+{
+	if ((i>=nrow(m))||(j>=ncol(m)))
+	{
+		LATAN_ERROR_VOID("index out of range",LATAN_EBADLEN);
+	}
+
+	gsl_matrix_set(m,i,j,val);
+}
+
+int mat_cp_subm(mat m, const mat n, const size_t k1, const size_t l1, \
+				const size_t k2, const size_t l2)
+{
+	gsl_matrix_view nview;
+	
+	if ((k2-k1+1>nrow(n))||(k2-k1+1<1)||(l2-l1+1>ncol(n))||(l2-l1+1<1))
+	{
+		LATAN_ERROR("invalid sub-matrix dimensions",LATAN_EBADLEN);
+	}
+	if ((k2-k1+1!=nrow(m))||(l2-l1+1!=ncol(m)))
+	{
+		LATAN_ERROR("sub-matrix and destination matrix dimensions do not match"\
+					,LATAN_EBADLEN);
+	}
+	
+	nview =	gsl_matrix_submatrix(n,(size_t)(k1),(size_t)(l1),			\
+								 (size_t)(k2-k1+1),(size_t)(l2-l1+1));
+	mat_cp(m,&(nview.matrix));
+	
+	return LATAN_SUCCESS;
+}
+
+int mat_set_subm(mat m, const mat n, const size_t k1, const size_t l1, \
+				 const size_t k2, const size_t l2)
+{
+	gsl_matrix_view mview;
+	
+	if ((k2-k1+1>nrow(m))||(k2-k1+1<1)||(l2-l1+1>ncol(m))||(l2-l1+1<1))
+	{
+		LATAN_ERROR("invalid sub-matrix dimensions",LATAN_EBADLEN);
+	}
+	if ((k2-k1+1!=nrow(n))||(l2-l1+1!=ncol(n)))
+	{
+		LATAN_ERROR("destination sub-matrix and matrix dimensions do not match"\
+					,LATAN_EBADLEN);
+	}
+	
+	mview =	gsl_matrix_submatrix(m,(size_t)(k1),(size_t)(l1),			\
+								 (size_t)(k2-k1+1),(size_t)(l2-l1+1));
+	mat_cp(&(mview.matrix),n);
+	
+	return LATAN_SUCCESS;
+}
+
+/* tests */
+bool mat_issamedim(const mat m, const mat n)
+{
+	return ((nrow(m) == nrow(n))&&(ncol(m) == ncol(n)));
+}
+
+bool mat_issquare(const mat m)
+{
+	return (nrow(m) == ncol(m));
+}
+
+/*								operations									*/
+/****************************************************************************/
+void mat_zero(mat m)
+{
+	gsl_matrix_set_zero(m);
+}
+
+void mat_cst(mat m, const double x)
+{
+	gsl_matrix_set_all(m,x);
+}
+
+int mat_cp(mat m, const mat n)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!mat_issamedim(m,n))
+	{
+		LATAN_ERROR("matrix copy with dimension mismatch",LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_memcpy(m,n));
+	
+	return status;
+}
+	
+int mat_eqadd(mat m, const mat n)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!(mat_issamedim(m,n)))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_add(m,n));
+	
+	return status;
+}
+
+int mat_add(mat m, const mat n, const mat o)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,mat_cp(m,n));
+	LATAN_UPDATE_STATUS(status,mat_eqadd(m,o));
+	
+	return status;
+}
+
+
+int mat_eqsub(mat m, const mat n)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!(mat_issamedim(m,n)))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_sub(m,n));
+	
+	return status;
+}
+
+int mat_sub(mat m, const mat n, const mat o)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,mat_cp(m,n));
+	LATAN_UPDATE_STATUS(status,mat_eqsub(m,o));
+	
+	return status;
+}
+		
+int mat_mul(mat m, const mat n, const mat o)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!((nrow(m) == nrow(n))&&(ncol(m) == ncol(o))&&(ncol(n) == nrow(o))))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,\
+											  1.0,n,o,0.0,m));
+	
+	return status;
+}
+
+int mat_eqmulp(mat m, const mat n)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!(mat_issamedim(m,n)))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_mul_elements(m,n));
+	
+	return status;
+}
+
+int mat_mulp(mat m, const mat n, const mat o)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,mat_cp(m,n));
+	LATAN_UPDATE_STATUS(status,mat_eqmulp(m,o));
+	
+	return status;
+}
+
+int mat_eqmuls(mat m, const double s)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_scale(m,s));
+	
+	return status;
+}
+
+int mat_muls(mat m, const mat n, const double s)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,mat_cp(m,n));
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(m,s));
+	
+	return status;
+}
+
+int mat_eqdivp(mat m, const mat n)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	if (!(mat_issamedim(m,n)))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	LATAN_UPDATE_STATUS(status,gsl_matrix_div_elements(m,n));
+	
+	return status;
+}
+
+int mat_divp(mat m, const mat n, const mat o)
+{
+	int status;
+	
+	status = LATAN_SUCCESS;
+	
+	LATAN_UPDATE_STATUS(status,mat_cp(m,n));
+	LATAN_UPDATE_STATUS(status,mat_eqdivp(m,o));
+	
+	return status;
+}
+
+int mat_abs(mat m, const mat n)
+{
+	size_t i,j;
+	
+	if (!mat_issamedim(m,n))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	FOR_VAL(m,i,j)
+	{
+		mat_set(m,i,j,fabs(mat_get(n,i,j)));
+	}
+	
+	return LATAN_SUCCESS;
+}
+
+int mat_sqrt(mat m, const mat n)
+{
+	size_t i,j;
+	
+	if (!mat_issamedim(m,n))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	FOR_VAL(m,i,j)
+	{
+		mat_set(m,i,j,sqrt(mat_get(n,i,j)));
+	}
+	
+	return LATAN_SUCCESS;
+}
+
+int mat_mean(mat mean, const mat *m, const size_t size)
+{
+	int status;
+	size_t i;
+	
+	status = LATAN_SUCCESS;
+	mat_zero(mean);
+	
+	for (i=0;i<size;i++)
+	{
+		LATAN_UPDATE_STATUS(status,mat_eqadd(mean,m[i]));
+	}
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(mean,1.0/((double)size)));
+	
+	return status;
+}
+
+int mat_meansig(mat mean, mat sig, const mat *m, const size_t size)
+{
+	int status;
+	size_t i;
+	const double dsize = ((double)(size));
+	mat sq;
+	
+	if (!mat_issamedim(mean,sig))
+	{
+		LATAN_ERROR("operation between matrices with dimension mismatch",\
+					LATAN_EBADLEN);
+	}
+	
+	status = LATAN_SUCCESS;
+	mat_zero(mean);
+	mat_zero(sig);
+	
+	mat_create(&sq,nrow(mean),ncol(mean));
+	
+	for (i=0;i<size;i++)
+	{
+		LATAN_UPDATE_STATUS(status,mat_eqadd(mean,m[i]));
+		LATAN_UPDATE_STATUS(status,mat_mulp(sq,m[i],m[i]));
+		LATAN_UPDATE_STATUS(status,mat_eqadd(sig,sq));
+	}
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(mean,1.0/dsize));
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(sig,1.0/(dsize-1.0)));
+	LATAN_UPDATE_STATUS(status,mat_mulp(sq,mean,mean));
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(sq,(dsize)/(dsize-1.0)));
+	LATAN_UPDATE_STATUS(status,mat_eqsub(sig,sq));
+	LATAN_UPDATE_STATUS(status,mat_eqsqrt(sig));
+	
+	mat_destroy(&sq);
+	
+	return status;
+}
+
+double mat_elsum(mat m)
+{
+	size_t i,j;
+	double sum;
+	
+	sum = 0.0;
+	
+	FOR_VAL(m,i,j)
+	{
+		sum += mat_get(m,i,j);
+	}
+	
+	return sum;
+}
+
+double mat_elmean(mat m)
+{
+	double mean;
+	
+	mean = mat_elsum(m)/((double)(nrow(m)*ncol(m)));
+	
+	return mean;
+}
+
+/*									I/O										*/
+/****************************************************************************/
+void mat_dump(FILE *stream, const mat m)
+{
+	size_t i,j;
+	
+	for (i=0;i<nrow(m);i++)
+	{
+		for (j=0;j<ncol(m)-1;j++)
+		{
+			fprintf(stream,"%.10e ",mat_get(m,i,j));
+		}
+		fprintf(stream,"%.10e\n",mat_get(m,i,ncol(m)-1));
+	}
+}
---
+++ latan/mat.h
@@ -0,0 +1,210 @@
+/* Main Doxygen page */
+/*!
+ @mainpage LatAnalyze - C library for lattice QCD results analysis
+ @author <A HREF=mailto:antonin.portelli@gmail.com>Antonin Portelli</A>
+ 
+ @section seci_nst 1 - Installation
+ To compile LatAnalyze you need only the <A HREF=http://gcc.gnu.org/>GNU C compiler</A> and C standard library. If you want to compile the documentation,
+ you will need <A HREF=http://www.doxygen.nl>Doxygen</A> and a <A HREF=http://www.latex-project.org/>LaTeX</A> distribution. 
+ <A HREF=http://www.gnuplot.info/>gnuplot</A> is not needed to compile the library, but it must be present at run time if you use functions from plot.h
+ header. Once this environment is set :
+ -# download the source of the library at http://dakant.hd.free.fr/latanalyze/latan-1.1.tar.gz :<BR>
+ <TT>$ wget http://dakant.hd.free.fr/latanalyze/latan-1.1.tar.gz</TT>
+ -# untar the <TT>latan-*.tar.gz</TT> archive :<BR>
+ <TT>$ tar -xvzf latan-*.tar.gz</TT>
+ -# go in the created directory :<BR>
+ <TT>$ cd latan-*</TT>
+ -# check that the head of <TT>Makefile</TT> match your system configuration
+ -# compile the library :<BR>
+ <TT>$ make lib</TT>
+ -# finally install the library :<BR>
+ <TT>$ make install</TT>
+ -# (optional) compile the documentation, it will be generated in three formats, HTML \htmlonly (this document) \endhtmlonly in <TT>doc/html</TT>, PDF
+ \latexonly (this document) \endlatexonly in <TT>doc/latex</TT> and Unix man \manonly (this document) \endmanonly in <TT>doc/man</TT> :<BR>
+ <TT>$ make doc</TT>
+ -# (optional) compile the examples given with the documentation, it's a good way to test that the library is working, the compiled example
+ executables will be in <TT>doc/examples</TT> :<BR>
+ <TT>$ make examples</TT>
+ 
+ You can uninstall LatAnalyze library with the <TT>make uninstall</TT> command.
+ 
+ @section sec_usage 2 - Usage
+ Include the needed LatAnalyze headers in your C code with include macros like :
+ \code #include <latan/xxx.h> \endcode
+ where <TT>xxx.h</TT> is the header you want to use. Eventualy use gcc <TT>-I</TT> option to indicate the LatAnalyze header files location at compilation. 
+ You also need to link your final executable with LatAnalyze with the gcc <TT>-llatan</TT> option, eventualy use gcc <TT>-L</TT> option to indicate 
+ the LatAnalyze library location for linking.
+ d6rtdddfrdrf
+ @section sec_history 3 - History
+ @subsection v1_0 v1.0 (2009-11-09)
+ First effective and tested version including initial features :
+ \arg dynamically allocated matrix type with basic arithmetic operations (see mat.h)
+ \arg I/O functions to read and save matrices in files (see io.h)
+ \arg bootstrap error evaluation function using array of matrices as data (see bootstrap.h)
+ @subsection v1_01 v1.01 (2009-11-10)
+ \arg FIX : ::mat_meansig was calculating variance and not standard deviation
+ \arg ADD : "coefficient by coefficient" square root operation on matrix added (see ::mat_sqrt and #mat_eqsqrt)
+ @subsection v1_1 v1.1 (2009-11-14)
+ \arg FIX : several memory leaks and allocation bugs
+ \arg ADD : <A HREF=http://www.gnuplot.info/>gnuplot</A> interface based on Nicolas Devillard <A HREF=http://ndevilla.free.fr/gnuplot/>gnuplot C API</A>
+ (see plot.h)
+ \arg ADD : function to compute effective mass from propagator datas (see ::effmass)
+*/
+/*!
+ @file mat.h
+ @brief Matrix of double precision float numbers structure implementation.
+ 
+ See io.h for reading and saving matrices on files.
+ @author <A HREF=mailto:antonin.portelli@gmail.com>Antonin Portelli</A> (<A HREF=http://www.cpt.univ-mrs.fr/>CPT</A>)
+*/
+/*!
+ @example ex_mat.c Manipulating mat variables.
+*/
+#ifndef LATAN_MAT_H_
+#define LATAN_MAT_H_
+
+#include <latan/globals.h>
+#include <gsl/gsl_matrix.h>
+
+__BEGIN_DECLS
+
+/* definition of the matrix type */
+typedef gsl_matrix* mat;
+
+/* loop */
+#define FOR_VAL(m,i,j)\
+for (i=0;i<nrow(m);i++)\
+	for (j=0;j<ncol(m);j++)\
+
+/* functions */
+/** allocation **/
+void mat_create(mat* m, const size_t init_nrow, const size_t init_ncol);
+void mat_create_ar(mat** m, const size_t nmat, const size_t init_nrow,\
+				   const size_t init_ncol);
+void mat_destroy(mat* m);
+void mat_destroy_ar(mat** m, const size_t nmat);
+
+/** access **/
+size_t nrow(const mat m);
+size_t ncol(const mat m);
+double mat_get(const mat m, const size_t i, const size_t j);
+void mat_set(mat m, const size_t i, const size_t j, const double val);
+int mat_set_subm(mat m, const mat n, const size_t k1, const size_t l1, \
+				 const size_t k2, const size_t l2);
+
+/** tests **/
+/*!
+ @fn BOOL mati_ssamedim(mat m, mat n)
+ @brief Compare dimensions of two matrices.
+ 
+ @return #TRUE is \b m got the same dimensions than \b n, #FALSE else.
+*/
+bool mat_issamedim(mat m, mat n);
+/*!
+ @fn BOOL mati_ssquare(mat m)
+ @brief Check if a matrix is square.
+ 
+ @return #TRUE if \b m is square, #FALSE else.
+*/
+bool mat_issquare(mat m);
+
+/** operations **/
+void mat_zero(mat m);
+void mat_cst(mat m, const double x);
+/*!
+ @fn void mat_cp(mat m, const mat n)
+ @brief Copy the content of the matrix \b n in \b m.
+ 
+ @warning \b m and \b n must have the same dimensions, fatal error will be generated if it is not the case.
+ */
+int mat_cp(mat m, const mat n);
+int mat_cp_subm(mat m, const mat n, const size_t k1, const size_t l1, \
+				const size_t k2, const size_t l2);
+int mat_eqadd(mat m, const mat n);
+int mat_add(mat m, const mat n, const mat o);
+int mat_eqsub(mat m, const mat n);
+int mat_sub(mat m, const mat n, const mat o);
+#define mat_eqmul(m,n) mat_mul(m,m,n)
+/*!
+ @fn void mat_mul(mat m, const mat n, const mat o)
+ @brief Store in \b m the matrix product of \b n and \b o.
+ @see #mat_eqmul
+ 
+ @warning \b NCOL(n) and \b NROW(o) must be equals, \b m must have the same number of rows than \b n and the same
+ number of columns than \b o. Fatal error will be generated if precedent conditions are not satisfied.
+ */
+int mat_mul(mat m, const mat n, const mat o);
+/*!
+ @fn void mat_eqmulp(mat m, const mat n)
+ @brief Multiply \b m by \b n "coefficient by coefficient".
+ @see #mat_mulp
+ 
+ @warning \b m and \b n must have the same dimensions, fatal error will be generated if it is not the case.
+*/
+int mat_eqmulp(mat m, const mat n);
+int mat_mulp(mat m, const mat n, const mat o);
+int mat_eqmuls(mat m, const double s);
+int mat_muls(mat m, const mat n, const double s);
+int mat_eqdivp(mat m, const mat n);
+int mat_divp(mat m, const mat n, const mat o);
+#define mat_eqabs(m) mat_abs(m,m)
+int mat_abs(mat m, const mat n);
+/*!
+ @def mat_eqsqrt(m)
+ @brief Compute the "coefficient by coefficient" square root of \b m.
+ @see ::mat_sqrt
+ */
+#define mat_eqsqrt(m) mat_sqrt(m,m)
+/*!
+ @fn void mat_sqrt(mat m, const mat n)
+ @brief Store in \b m the "coefficient by coefficient" square root of \b n.
+ @see #mat_eqsqrt
+ 
+ @warning \b m and \b n must have the same dimensions, fatal error will be generated if it is not the case.
+*/
+int mat_sqrt(mat m, const mat n);
+/*!
+ @fn void mat_mean(mat mean, const mat *m, const int size)
+ @brief Compute the "coefficient by coefficient" mean of an array of matrices.
+ @see #mat_print
+ 
+ @param mean matrix to store the mean
+ @param m input array of matrices
+ @param size number of matrices in the array
+ @warning \b mean and all the elements of \b m must have the same dimensions, fatal error will be generated if it is not the case.
+ */
+int mat_mean(mat mean, const mat *m, const size_t size);
+/*!
+ @fn void mat_meansig(mat mean, mat sig, const mat *m, const int size)
+ @brief Compute the "coefficient by coefficient" mean and standard deviation of an array of matrices.
+ 
+ @param mean matrix to store the mean
+ @param sig matrix to store the standard deviation
+ @param m input array of matrices
+ @param size number of matrices in the array
+ @warning \b mean, \b sig and all the elements of \b m must have the same dimensions, fatal error will be generated if it is not the case.
+ */
+int mat_meansig(mat mean, mat sig, const mat *m, const size_t size);
+
+/** I/O **/
+/*!
+ @fn void mat_dump(FILE *stream, mat m)
+ @brief Print the coefficients of a matrix on a character stream. Columns are separated by a space character and rows 
+ are separated by a new line character. Value are printed in scientific notation with 11 digits.
+ 
+ @param stream character stream where the matrix will be printed
+ @param m matrix to print
+*/
+double mat_elsum(mat m);
+double mat_elmean(mat m);
+void mat_dump(FILE *stream, mat m);
+/*!
+ @def mat_print(m)
+ @brief Print \b m on the standard output.
+ @see ::mat_dump
+ */
+#define mat_print(m) mat_dump(stdout,m)
+
+__END_DECLS
+
+#endif

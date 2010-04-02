94a12eb11cb75d62761012d274d58f8c2680724c
Antonin Portelli <antonin.portelli@me.com>
2010-04-02 13:30:31 +0200

    new fopen and allocation macros + strinbuf minor tweak

--- latan/globals.c
+++ latan/globals.c
@@ -1,5 +1,5 @@
 #include <latan/globals.h>
 #include <latan/includes.h>
 
-const char* latan_name = PACKAGE_NAME;
-const char* latan_version = PACKAGE_VERSION;
+const stringbuf latan_name = PACKAGE_NAME;
+const stringbuf latan_version = PACKAGE_VERSION;

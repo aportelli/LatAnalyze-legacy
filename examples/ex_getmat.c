225af516b8a382e5acf65b5a15cc7a09d1d9a2e8
Antonin Portelli <antonin.portelli@me.com>
2010-04-06 11:29:14 +0200

    examples update

--- examples/ex_getmat.c
+++ examples/ex_getmat.c
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
     mat a,b;
 	int dat_nrow;
 	
-	dat_nrow = get_nrow("FOO","id1","ex_getmat_f1");
+	dat_nrow = mat_load_nrow("FOO","id1","ex_getmat_f1");
 	mat_create(&a,(size_t)(dat_nrow),2);
-	dat_nrow = get_nrow("BAR","testid","ex_getmat_f1");
+	dat_nrow = mat_load_nrow("BAR","testid","ex_getmat_f1");
 	mat_create(&b,(size_t)(dat_nrow),2);
 	printf("Getting a from FOO id1 in file ex_getmat_f1...\n");
-	get_mat(a,"FOO","id1","ex_getmat_f1");
+	mat_get(a,"FOO","id1","ex_getmat_f1");
 	printf("Getting b from BAR testid in file ex_getmat_f1...\n\n");
-	get_mat(b,"BAR","testid","ex_getmat_f1");
+	mat_get(b,"BAR","testid","ex_getmat_f1");
 	printf("a =\n");
 	mat_print(a);
 	printf("\nb =\n");
@@ -31,10 +31,10 @@ int main(int argc, char *argv[])
 	nfile = get_nfile("ex_getmat_man");
 	
 	get_firstfname(ffname,"ex_getmat_man");
-	dat_nrow = get_nrow("FOO","id1",ffname);
+	dat_nrow = mat_load_nrow("FOO","id1",ffname);
 	mat_create_ar(&c,(size_t)(nfile),(size_t)(dat_nrow),2);
 	printf("Filling array c from FOO id1 matrices in files referenced in file ex_getmat_man...\n\n");
-	get_matn(c,"FOO","id1","ex_getmat_man");
+	mat_get_ar(c,"FOO","id1","ex_getmat_man");
 	for (i=0;i<nfile;i++)
 	{
 		printf("c[%i] =\n",i);
--- examples/ex_mat.c
+++ examples/ex_mat.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <latan/mat.h>
+#include <latan/rand.h>
+#include <latan/io.h>
 
 int main(int argc, char *argv[])
 {
-	mat a,b,c,d;
+	mat a,b,c,d,e,f;
 	
-	mat_create(&a,3,2);
+	mat_create(&a,3,3);
 	mat_create(&b,3,1);
-	mat_create(&c,1,2);
+	mat_create(&c,1,3);
 	mat_create(&d,2,2);
+	mat_create(&e,3,3);
+	
+	rand_timeinit();
 	
 	mat_set(b,0,0,2.0);
+	mat_set(b,1,0,1.5);
 	mat_set(b,2,0,5.0);
 	mat_set(c,0,0,1.0);
-	mat_set(c,0,1,3.0);
+	mat_set(c,0,1,10.0);
+	mat_set(c,0,2,6.7);
 	printf("b =\n");
 	mat_print(b);
 	printf("c =\n");
@@ -27,14 +34,27 @@ int main(int argc, char *argv[])
 	printf("d =\n");
 	mat_print(d);
 	printf("a(1,0:1) <- c\n");
-	mat_set_subm(a,c,1,0,1,1);
+	mat_set_subm(a,c,1,0,1,2);
 	printf("a =\n");
 	mat_print(a);
+	printf("a <- random(-6,6)\n");
+	mat_rand_u(a,-6.0,6.0);
+	printf("a =\n");
+	mat_print(a);
+	printf("e <- a^(-1)\n");
+	mat_inv(e,a);
+	printf("e =\n");
+	mat_print(e);
+	printf("e <- e*a\n");
+	mat_eqmul_r(e,a);
+	printf("e =\n");
+	mat_print(e);
 	
 	mat_destroy(&a);
 	mat_destroy(&b);
 	mat_destroy(&c);
 	mat_destroy(&d);
+	mat_destroy(&e);
 	
 	return EXIT_SUCCESS;
 }

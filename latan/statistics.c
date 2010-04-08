7e12efbd18c037cd7f9757a3f93242a39adcbd9b
Antonin Portelli <antonin.portelli@me.com>
2010-04-08 07:28:44 +0200

    covariance calculation functions

--- latan/statistics.c
+++ latan/statistics.c
@@ -32,6 +32,7 @@ int mat_mean(mat mean, const mat *m, const size_t size)
 {
 	int status;
 	size_t i;
+	const double dsize = (double)(size);
 	
 	status = LATAN_SUCCESS;
 	mat_zero(mean);
@@ -40,44 +41,106 @@ int mat_mean(mat mean, const mat *m, const size_t size)
 	{
 		LATAN_UPDATE_STATUS(status,mat_eqadd(mean,m[i]));
 	}
-	LATAN_UPDATE_STATUS(status,mat_eqmuls(mean,1.0/((double)size)));
+	LATAN_UPDATE_STATUS(status,mat_eqmuls(mean,1.0/dsize));
 	
 	return status;
 }
 
-int mat_meansig(mat mean, mat sig, const mat *m, const size_t size)
+int mat_cov(mat cov, const mat *m, const mat *n, const size_t size)
+{
+	int status;
+	mat m_mean, n_mean;
+	
+	status = LATAN_SUCCESS;
+	
+	mat_create_from_dim(&m_mean,cov);
+	mat_create_from_dim(&n_mean,cov);
+	
+	LATAN_UPDATE_STATUS(status,mat_mean(m_mean,m,size));
+	LATAN_UPDATE_STATUS(status,mat_mean(n_mean,n,size));
+	LATAN_UPDATE_STATUS(status,mat_cov_m(cov,m,n,size,m_mean,n_mean));
+	
+	mat_destroy(&m_mean);
+	mat_destroy(&n_mean);
+	
+	return status;
+}
+
+int mat_cov_m(mat cov, const mat* m, const mat* n, const size_t size,\
+			  const mat m_mean, const mat n_mean)
 {
 	int status;
 	size_t i;
-	const double dsize = ((double)(size));
-	mat sq;
-	
-	if (!mat_issamedim(mean,sig))
-	{
-		LATAN_ERROR("operation between matrices with dimension mismatch",\
-					LATAN_EBADLEN);
-	}
+	size_t subdim;
+	double dsubdim;
+	mat* mtn;
+	mat mean_tprod;
 	
 	status = LATAN_SUCCESS;
-	mat_zero(mean);
-	mat_zero(sig);
+	subdim = ncol(m[0]);
+	dsubdim = (double)(subdim);
 	
-	mat_create(&sq,nrow(mean),ncol(mean));
+	mat_create_ar_from_dim(&mtn,size,cov);
+	mat_create_from_dim(&mean_tprod,cov);
+	
+	for (i=0;i<size;i++) 
+	{
+		LATAN_UPDATE_STATUS(status,mat_mul_nt(mtn[i],m[i],n[i]));
+		LATAN_UPDATE_STATUS(status,mat_eqmuls(mtn[i],1.0/dsubdim));
+	}
+	LATAN_UPDATE_STATUS(status,mat_mul_nt(mean_tprod,m_mean,n_mean));
+	LATAN_UPDATE_STATUS(status,mat_mean(cov,mtn,size));
+	LATAN_UPDATE_STATUS(status,mat_eqsub(cov,mean_tprod));
+	
+	mat_destroy_ar(&mtn,size);
+	mat_destroy(&mean_tprod);
+	
+	return status;
+}
+
+int mat_covp(mat cov, const mat *m, const mat *n, const size_t size)
+{
+	int status;
+	mat m_mean, n_mean;
+	
+	status = LATAN_SUCCESS;
+	
+	mat_create_from_dim(&m_mean,cov);
+	mat_create_from_dim(&n_mean,cov);
+	
+	LATAN_UPDATE_STATUS(status,mat_mean(m_mean,m,size));
+	LATAN_UPDATE_STATUS(status,mat_mean(n_mean,n,size));
+	LATAN_UPDATE_STATUS(status,mat_covp_m(cov,m,n,size,m_mean,n_mean));
+	
+	mat_destroy(&m_mean);
+	mat_destroy(&n_mean);
+	
+	return status;
+}
+
+int mat_covp_m(mat cov, const mat* m, const mat* n, const size_t size,\
+				const mat m_mean, const mat n_mean)
+{
+	int status;
+	size_t i;
+	mat* mn;
+	mat mean_prod;
+	
+	status = LATAN_SUCCESS;
+	
+	mat_create_ar_from_dim(&mn,size,cov);
+	mat_create_from_dim(&mean_prod,cov);
 	
 	for (i=0;i<size;i++)
 	{
-		LATAN_UPDATE_STATUS(status,mat_eqadd(mean,m[i]));
-		LATAN_UPDATE_STATUS(status,mat_mulp(sq,m[i],m[i]));
-		LATAN_UPDATE_STATUS(status,mat_eqadd(sig,sq));
+		LATAN_UPDATE_STATUS(status,mat_mulp(mn[i],m[i],n[i]));
 	}
-	LATAN_UPDATE_STATUS(status,mat_eqmuls(mean,1.0/dsize));
-	LATAN_UPDATE_STATUS(status,mat_eqmuls(sig,1.0/(dsize-1.0)));
-	LATAN_UPDATE_STATUS(status,mat_mulp(sq,mean,mean));
-	LATAN_UPDATE_STATUS(status,mat_eqmuls(sq,(dsize)/(dsize-1.0)));
-	LATAN_UPDATE_STATUS(status,mat_eqsub(sig,sq));
-	LATAN_UPDATE_STATUS(status,mat_eqsqrt(sig));
+	LATAN_UPDATE_STATUS(status,mat_mulp(mean_prod,m_mean,n_mean));
+	LATAN_UPDATE_STATUS(status,mat_mean(cov,mn,size));
+	LATAN_UPDATE_STATUS(status,mat_eqsub(cov,mean_prod));
 	
-	mat_destroy(&sq);
+	mat_destroy_ar(&mn,size);
+	mat_destroy(&mean_prod);
 	
 	return status;
 }
--- latan/statistics.h
+++ latan/statistics.h
@@ -26,7 +26,7 @@ double mat_elmean(mat m);
  @param size number of matrices in the array
  @warning \b mean and all the elements of \b m must have the same dimensions, fatal error will be generated if it is not the case.
  */
-int mat_mean(mat mean, const mat *m, const size_t size);
+int mat_mean(mat mean, const mat* m, const size_t size);
 /*!
  @fn void mat_meansig(mat mean, mat sig, const mat *m, const int size)
  @brief Compute the "coefficient by coefficient" mean and standard deviation of an array of matrices.
@@ -37,15 +37,21 @@ int mat_mean(mat mean, const mat *m, const size_t size);
  @param size number of matrices in the array
  @warning \b mean, \b sig and all the elements of \b m must have the same dimensions, fatal error will be generated if it is not the case.
  */
-int mat_meansig(mat mean, mat sig, const mat *m, const size_t size);
+int mat_cov(mat cov, const mat* m, const mat* n, const size_t size);
+int mat_cov_m(mat cov, const mat* m, const mat* n, const size_t size,\
+			  const mat m_mean, const mat n_mean);
+#define mat_var(var,m,size) mat_cov(var,m,m,size)
+#define mat_var_m(var,m,size,mean) mat_cov(var,m,m,size,mean,mean)
+int mat_covp(mat cov, const mat* m, const mat* n, const size_t size);
+int mat_covp_m(mat cov, const mat* m, const mat* n, const size_t size,\
+			   const mat m_mean, const mat n_mean);
+#define mat_varp(var,m,size) mat_covar(var,m,m,size)
+#define mat_varp_m(var,m,size,mean) mat_covar_m(var,m,m,size,mean,mean)
 
 /* resampling functions */
-int resamp_bootstrap(mat res, mat sigres, mat* dat, size_t ndat,\
-					 estimator_t* estimator, void* param,		\
-					 size_t nboot, const boot_io* io_param);
-int resamp_jackknife(mat res, mat sigres, mat* dat, size_t ndat,\
+int resamp_bootstrap(boot_res* res, const mat* dat, size_t ndat,\
 					 estimator_t* estimator, void* param);
-
-extern const boot_io boot_no_io;
+int resamp_jackknife(jack_res* sample, const mat* dat, size_t ndat,	\
+					 estimator_t* estimator, void* param);
 
 #endif

652e4597ce7398593c53135172e19bae892e6db9
Antonin Portelli <antonin@MacAnt.local>
2010-04-23 16:41:03 +0200

    recoding of hadron and spectrum objects

---
+++ latan/spectrum.h
@@ -0,0 +1,59 @@
+#ifndef LATAN_SPECTRUM_H_
+#define LATAN_SPECTRUM_H_
+
+#include <latan/globals.h>
+#include <latan/hadron.h>
+
+__BEGIN_DECLS
+
+/* channels */
+typedef enum
+{
+	ch_SS		= 0,
+	ch_VV		= 1,
+	ch_PP		= 2,
+	ch_PA		= 3,
+	ch_AP		= 4,
+	ch_AA		= 5,
+	ch_N		= 6,
+	ch_Lambda	= 7,
+	ch_Delta	= 8
+} channel_no;
+
+void channel_id_set(const channel_no i, const stringbuf new_id);
+void channel_id_get(stringbuf str, const channel_no i);
+
+/* quarks */
+typedef enum
+{
+	qu_l	= 0,
+	qu_u	= 1,
+	qu_d	= 2,
+	qu_s	= 3
+} quark_no;
+
+void quark_id_set(const quark_no i, const stringbuf new_id);
+void quark_id_get(stringbuf str, const quark_no i);
+void diquark_id_get(stringbuf str, const quark_no i1, const quark_no i2);
+void triquark_id_get(stringbuf str, const quark_no i1, const quark_no i2,\
+					 const quark_no i3);
+
+/* spectrum type */
+typedef struct
+{
+	hadron* particle;
+	size_t nparticle;
+}* spectrum;
+
+/** allocation **/
+spectrum spectrum_create(const size_t nparticle);
+spectrum spectrum_create_qcd(void);
+spectrum spectrum_create_qcdqed(void);
+void spectrum_destroy(spectrum s);
+
+/** access **/
+hadron spectrum_get(const spectrum s, const stringbuf part_name);
+
+__END_DECLS
+
+#endif

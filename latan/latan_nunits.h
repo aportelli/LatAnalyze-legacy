3e48a435b5154455d624f56ae800ef9b0cef9a91
Antonin Portelli <antonin@MacAnt.local>
2010-05-20 19:56:27 +0200

    more NU constants

--- latan/latan_nunits.h
+++ latan/latan_nunits.h
@@ -1,6 +1,21 @@
 #ifndef LATAN_NUNITS_H_
 #define LATAN_NUNITS_H_
 
-#define NU_FM 0.005067731	/* MeV^(-1) */
+/* source : Review of Particle Physics 2009 (http://pdglive.lbl.gov/) */
+
+/* units */
+#define NU_FM				0.005067731				/* MeV^(-1) */
+
+/* constants */
+#define NU_ALPHA_EM			7.2973525376e-3			/* no dim */
+#define NU_ELECTRON_CHARGE	0.302822120214353		/* no dim */
+
+/* masses */
+#define NU_M_PI_P			139.57018				/* MeV */
+#define NU_M_PI_P_ERR		3.5e-4					/* MeV */
+#define NU_M_PI_0			134.9766				/* MeV */
+#define NU_M_PI_0_ERR		6e-4					/* MeV */
+#define NU_M_PI_M			139.57018				/* MeV */
+#define NU_M_PI_M_ERR		3.5e-4					/* MeV */
 
 #endif

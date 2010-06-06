b96ea1e359935e4eb9eead1d711a0db5e8709a16
Antonin Portelli <antonin@MacAnt.local>
2010-06-06 12:11:57 +0200

    2D polynomial fitting models

--- latan/latan_minimizer.c
+++ latan/latan_minimizer.c
@@ -132,6 +132,233 @@ const fit_model fm_cosh =
 	"cosh(%e*x)*%e"
 };
 
+double fm_polyn_2d_00_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_00 =
+{
+	"z(x,y) = p_0",
+	&fm_polyn_2d_00_func,
+	1,
+	2,
+	"%e"
+};
+
+double fm_polyn_2d_01_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*y;
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_01 =
+{
+	"z(x,y) = p_0+p_1*y",
+	&fm_polyn_2d_01_func,
+	2,
+	2,
+	"%e+%e*y"
+};
+
+double fm_polyn_2d_02_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*y         \
+	      + mat_get(func_param,2,0)*SQ(y);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_02 =
+{
+	"z(x,y) = p_0+p_1*y+p_2*y^2",
+	&fm_polyn_2d_02_func,
+	3,
+	2,
+	"%e+%e*y+%e*y**2"
+};
+
+double fm_polyn_2d_10_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+		  + mat_get(func_param,1,0)*x;
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_10 =
+{
+	"z(x,y) = p_0+p_1*x",
+	&fm_polyn_2d_10_func,
+	2,
+	2,
+	"%e+%e*x"
+};
+
+double fm_polyn_2d_11_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*x         \
+	      + mat_get(func_param,2,0)*y         \
+	      + mat_get(func_param,3,0)*x*y;
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_11 =
+{
+	"z(x,y) = p_0+p_1*x+p_2*y+p_3*x*y",
+	&fm_polyn_2d_11_func,
+	4,
+	2,
+	"%e+%e*x+%e*y+%e*x*y"
+};
+
+double fm_polyn_2d_12_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*x         \
+	      + mat_get(func_param,2,0)*y         \
+	      + mat_get(func_param,3,0)*x*y       \
+	      + mat_get(func_param,4,0)*SQ(y);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_12 =
+{
+	"z(x,y) = p_0+p_1*x+p_2*y+p_3*x*y+p_4*y^2",
+	&fm_polyn_2d_12_func,
+	5,
+	2,
+	"%e+%e*x+%e*y+%e*x*y+%e*y**2"
+};
+
+double fm_polyn_2d_20_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*x         \
+	      + mat_get(func_param,2,0)*SQ(x);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_20 =
+{
+	"z(x,y) = p_0+p_1*x+p_2*x^2",
+	&fm_polyn_2d_20_func,
+	3,
+	2,
+	"%e+%e*x+%e*x**2"
+};
+
+double fm_polyn_2d_21_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;	
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*x         \
+	      + mat_get(func_param,2,0)*y         \
+	      + mat_get(func_param,3,0)*x*y       \
+	      + mat_get(func_param,4,0)*SQ(x);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_21 =
+{
+	"z(x,y) = p_0+p_1*x+p_2*y+p_3*x*y+p_4*x^2",
+	&fm_polyn_2d_21_func,
+	5,
+	2,
+	"%e+%e*x+%e*y+%e*x*y+%e*x**2"
+};
+
+double fm_polyn_2d_22_func(const mat X, const mat func_param, void* nothing)
+{
+	double res,x,y;
+	
+	nothing = NULL;
+	x = mat_get(X,0,0);
+	y = mat_get(X,0,1);
+	
+	res = mat_get(func_param,0,0)             \
+	      + mat_get(func_param,1,0)*x         \
+	      + mat_get(func_param,2,0)*y         \
+	      + mat_get(func_param,3,0)*x*y       \
+	      + mat_get(func_param,4,0)*SQ(x)     \
+	      + mat_get(func_param,5,0)*SQ(y);
+	
+	return res;
+}
+
+const fit_model fm_polyn_2d_22 =
+{
+	"z(x,y) = p_0+p_1*x+p_2*y+p_3*x*y+p_4*x^2+p_5*y_2",
+	&fm_polyn_2d_22_func,
+	6,
+	2,
+	"%e+%e*x+%e*y+%e*x*y+%e*x**2+%e*y**2"
+};
+
+const fit_model* fm_polyn_2d[3][3] =
+{
+	{&fm_polyn_2d_00, &fm_polyn_2d_01, &fm_polyn_2d_02},\
+	{&fm_polyn_2d_10, &fm_polyn_2d_11, &fm_polyn_2d_12},\
+	{&fm_polyn_2d_20, &fm_polyn_2d_21, &fm_polyn_2d_22}
+};
+
 /*							fit data structure								*/
 /****************************************************************************/
 /** allocation **/

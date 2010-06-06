834a9b7baf2a8aa26b5d5edbd27c98f6e837868d
Antonin Portelli <antonin.portelli@me.com>
2010-06-06 12:29:13 +0200

    usage message for util latan_create_rg_state

--- utils/latan_create_rg_state.c
+++ utils/latan_create_rg_state.c
@@ -8,6 +8,12 @@ int main(int argc, char* argv[])
 {
 	randgen_state state;
 	
+	if (argc != 2)
+	{
+		fprintf(stderr,"usage: %s <output_file>\n",argv[0]);
+		return EXIT_FAILURE;
+	}
+	
 	randgen_init_from_time();
 	randgen_get_state(state);
 	randgen_save_state(OUTF_NAME,state);

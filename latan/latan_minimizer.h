4cb4c507e8a98d1627db8e8beed63194227733f1
Antonin Portelli <antonin.portelli@me.com>
2010-06-06 12:11:57 +0200

    2D polynomial fitting models

--- latan/latan_minimizer.h
+++ latan/latan_minimizer.h
@@ -44,6 +44,26 @@ extern const fit_model fm_expdec;
 /*** hyperbolic cosine: y(x) = p0*cosh(p1*x) ***/
 double fm_cosh_func(const mat x, const mat func_param, void* nothing);
 extern const fit_model fm_cosh;
+/*** 2D polynomial models ***/
+double fm_polyn_2d_00_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_00;
+double fm_polyn_2d_01_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_01;
+double fm_polyn_2d_02_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_02;
+double fm_polyn_2d_10_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_10;
+double fm_polyn_2d_11_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_11;
+double fm_polyn_2d_12_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_12;
+double fm_polyn_2d_20_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_20;
+double fm_polyn_2d_21_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_21;
+double fm_polyn_2d_22_func(const mat X, const mat func_param,void* nothing);
+extern const fit_model fm_polyn_2d_22;
+extern const fit_model* fm_polyn_2d[3][3];
 
 /* fit data structure */
 typedef struct

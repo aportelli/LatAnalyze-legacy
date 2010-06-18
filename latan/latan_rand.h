17213a07782fb1ef138cbfa006acd3985b2e909c
Antonin Portelli <antonin@MacAnt.local>
2010-06-18 17:13:51 +0200

    randgen_state definition moved to rand module

--- latan/latan_rand.h
+++ latan/latan_rand.h
@@ -2,13 +2,15 @@
 #define LATAN_RAND_H_
 
 #include <latan/latan_globals.h>
-#include <latan/latan_statistics.h>
 
 __BEGIN_DECLS
 
+/* type for random generator state */
+#define RLXG_STATE_SIZE 105
+typedef int randgen_state[RLXG_STATE_SIZE];
+
 void randgen_init(const int seed);
 void randgen_init_from_time(void);
-latan_errno randgen_set_state_from_rs_sample(rs_sample s);
 void randgen_set_state(randgen_state state);
 void randgen_get_state(randgen_state state);
 double rand_u(double a, double b);

fecad6c1ad6cb1ce7889a36c9bbc6637b724570a
Antonin Portelli <antonin@MacAnt.local>
2010-06-19 10:52:04 +0200

    1D polynomial fitting models

--- latan/latan_models.h
+++ latan/latan_models.h
@@ -4,23 +4,34 @@
 #include <latan/latan_globals.h>
 #include <latan/latan_minimizer.h>
 
-/*** constant: y(x) = p0 ***/
-double fm_const_func(const mat x, const mat func_param, void* nothing);
-extern const fit_model fm_const;
+/* 1D models */
+/** 1D polynomial models **/
+#define FM_POLYN_1D_MAXDEG 4
+double fm_polyn_1d_0_func(const mat X, const mat func_param, void* nothing);
+extern const fit_model fm_polyn_1d_0;
+double fm_polyn_1d_1_func(const mat X, const mat func_param, void* nothing);
+extern const fit_model fm_polyn_1d_1;
+double fm_polyn_1d_2_func(const mat X, const mat func_param, void* nothing);
+extern const fit_model fm_polyn_1d_2;
+double fm_polyn_1d_3_func(const mat X, const mat func_param, void* nothing);
+extern const fit_model fm_polyn_1d_3;
+double fm_polyn_1d_4_func(const mat X, const mat func_param, void* nothing);
+extern const fit_model fm_polyn_1d_4;
+#define fm_lin fm_polyn_1d_1
+#define fm_const fm_polyn_1d_0
+extern const fit_model* fm_polyn_1d[FM_POLYN_1D_MAXDEG+1];
 
-/*** linear: y(x) = p0 + p1*x ***/
-double fm_lin_func(const mat x, const mat func_param, void* nothing);
-extern const fit_model fm_lin;
-
-/*** exponential decay: y(x) = p0*exp(-p1*x) ***/
-double fm_expdec_func(const mat x, const mat func_param, void* nothing);
+/** exponential decay **/
+double fm_expdec_func(const mat X, const mat func_param, void* nothing);
 extern const fit_model fm_expdec;
 
-/*** hyperbolic cosine: y(x) = p0*cosh(p1*x) ***/
-double fm_cosh_func(const mat x, const mat func_param, void* nothing);
+/** hyperbolic cosine **/
+double fm_cosh_func(const mat X, const mat func_param, void* nothing);
 extern const fit_model fm_cosh;
 
-/*** 2D polynomial models ***/
+/* 2D models */
+/** 2D polynomial models **/
+#define FM_POLYN_2D_MAXDEG 2
 double fm_polyn_2d_00_func(const mat X, const mat func_param,void* nothing);
 extern const fit_model fm_polyn_2d_00;
 double fm_polyn_2d_01_func(const mat X, const mat func_param,void* nothing);
@@ -39,6 +50,6 @@ double fm_polyn_2d_21_func(const mat X, const mat func_param,void* nothing);
 extern const fit_model fm_polyn_2d_21;
 double fm_polyn_2d_22_func(const mat X, const mat func_param,void* nothing);
 extern const fit_model fm_polyn_2d_22;
-extern const fit_model* fm_polyn_2d[3][3];
+extern const fit_model* fm_polyn_2d[FM_POLYN_2D_MAXDEG+1][FM_POLYN_2D_MAXDEG+1];
 
 #endif

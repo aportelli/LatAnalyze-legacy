5a6987fd7167aa73d56ab53615a17d4594e84bde
Antonin Portelli <antonin.portelli@me.com>
2010-09-10 17:31:46 +0200

    kaon masses added

--- latan/latan_nunits.h
+++ latan/latan_nunits.h
@@ -11,11 +11,19 @@
 #define NU_ELECTRON_CHARGE	0.302822120214353		/* no dim */
 
 /* masses */
+/** pi **/
 #define NU_M_PI_P			139.57018				/* MeV */
 #define NU_M_PI_P_ERR		3.5e-4					/* MeV */
 #define NU_M_PI_0			134.9766				/* MeV */
 #define NU_M_PI_0_ERR		6.0e-4					/* MeV */
 #define NU_M_PI_M			139.57018				/* MeV */
 #define NU_M_PI_M_ERR		3.5e-4					/* MeV */
+/** K **/
+#define NU_M_K_P			493.677					/* MeV */
+#define NU_M_K_P_ERR		0.016					/* MeV */
+#define NU_M_K_0			497.614					/* MeV */
+#define NU_M_K_0_ERR		0.024					/* MeV */
+#define NU_M_K_M			493.677					/* MeV */
+#define NU_M_K_M_ERR		0.016					/* MeV */
 
 #endif

11e4a6c92226374c89c6b6b73ebd37894179efe5
Antonin Portelli <antonin.portelli@gmail.com>
2011-02-03 14:47:34 +0100

    propagator array functions abandoned

--- latan/latan_io_xml.h
+++ latan/latan_io_xml.h
@@ -37,14 +37,6 @@ latan_errno prop_load_xml(mat *prop, const channel_no channel, \
                           const quark_no q1, const quark_no q2,\
                           const ss_no source, const ss_no sink,\
                           strbuf fname);
-latan_errno prop_ar_load_nprop_xml(size_t *nprop, const channel_no channel,\
-                                   const quark_no q1, const quark_no q2,   \
-                                   const ss_no source, const ss_no sink,   \
-                                   strbuf manfname);
-latan_errno prop_ar_load_xml(mat **prop, const channel_no channel,\
-                             const quark_no q1, const quark_no q2,\
-                             const ss_no source, const ss_no sink,\
-                             strbuf manfname);
 latan_errno prop_save_xml(strbuf fname, const char mode, mat *prop, \
                           const strbuf channel,                     \
                           const quark_no q1, const quark_no q2,     \

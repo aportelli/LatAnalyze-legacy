ce256d37751ef1e5892c692269aab87e0d427333
Antonin Portelli <antonin.portelli@me.com>
2011-03-08 19:04:13 +0100

    build script include debug symbols for Mac OS X

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 make -j3 1>/dev/null
 sudo make uninstall 1>/dev/null
 sudo make install 1>/dev/null
+if [ `uname` == "Darwin" ]
+then
+	sudo dsymutil latan/.libs/liblatan.0.dylib --out=/usr/local/lib/liblatan.0.dylib.dSYM
+fi
+

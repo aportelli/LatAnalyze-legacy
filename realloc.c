d278246695bd4d8c714dfc3bec6c97474e0362de
Antonin Portelli <antonin.portelli@me.com>
2011-03-14 11:51:05 +0100

    minor fixes on allocator replacement functions

--- realloc.c
+++ realloc.c
@@ -1,8 +1,11 @@
 #include <config.h>
+#include <stdlib.h>
 #undef realloc
 #include <sys/types.h>
 
+void *malloc(size_t n);
 void *realloc(void *pt, size_t n);
+void *rpl_realloc (void *pt, size_t n);
 
 /* Reallocate an N-byte block of memory from the heap. If N is zero, allocate a 1-byte block. */
 void *rpl_realloc (void *pt, size_t n) 

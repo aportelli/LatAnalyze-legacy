e76c3d5a09495b0084f67161cd11e14246682c8a
Antonin Portelli <antonin.portelli@gmail.com>
2011-03-14 11:51:05 +0100

    minor fixes on allocator replacement functions

--- malloc.c
+++ malloc.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 
 void *malloc(size_t n);
+void *rpl_malloc(size_t n);
 
 /* Allocate an N-byte block of memory from the heap. If N is zero, allocate a 1-byte block. */
 void *rpl_malloc(size_t n) 

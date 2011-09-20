74f85426f05fcfffb595ec099f5c5ff354049bc0
Antonin Portelli <antonin.portelli@gmail.com>
2011-09-20 11:33:31 +0200

    more Linux compatibility fix

--- latan/latan_plot.c
+++ latan/latan_plot.c
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#define _POSIX_C_SOURCE 2
+
 #include <latan/latan_plot.h>
 #include <latan/latan_includes.h>
 

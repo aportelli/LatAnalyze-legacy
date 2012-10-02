b67f0b8dd5e5a3e06bdf06027aa79e661d4643e4
Antonin Portelli <antonin.portelli@me.com>
2012-10-02 17:09:05 +0100

    alloc replacement is not worth it

--- alloc.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALLOC_H_
-#define ALLOC_H_
-
-extern void *malloc(size_t n);
-extern void *realloc(void *pt, size_t n);
-void *rpl_malloc(size_t n);
-void *rpl_realloc (void *pt, size_t n);
-
-#endif
